a6d5f67f6f637c4d824e7c77e1af8ccf65c4f17c
damolaap <damolaap@gmail.com>
2024-07-24 13:31:00 +0100

    added a feature for custom post cover

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class BlogPost(models.Model):
+    blog_cover = models.ImageField(upload_to='blog_covers',null=True)
     blog_title = models.CharField(max_length=100)
     blog_content = models.TextField()
     blog_date= models.DateField()
